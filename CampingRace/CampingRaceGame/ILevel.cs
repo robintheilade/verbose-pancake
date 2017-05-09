803b29aaf0ea0d1656ed8b7744859f080f366cf6
Robin Theilade <robin.theilade@gmail.com>
2017-05-09 08:07:02 +0200

    Add comments to ILevel and Level

--- CampingRace/CampingRaceGame/ILevel.cs
+++ CampingRace/CampingRaceGame/ILevel.cs
@@ -2,8 +2,14 @@
 
 namespace CampingRaceGame
 {
+    /// <summary>
+    /// Represents a level in the game.
+    /// </summary>
     public interface ILevel
     {
+        /// <summary>
+        /// Gets or sets the array of <see cref="SceneObject"/> in the current level.
+        /// </summary>
         SceneObject[] SceneObjects
         {
             get;
--- CampingRace/CampingRaceGame/Level.cs
+++ CampingRace/CampingRaceGame/Level.cs
@@ -2,8 +2,14 @@
 
 namespace CampingRaceGame
 {
+    /// <summary>
+    /// Represents a level in the game.
+    /// </summary>
     public class Level : ILevel
     {
+        /// <summary>
+        /// Gets or sets the array of <see cref="SceneObject"/> in the current level.
+        /// </summary>
         public SceneObject[] SceneObjects
         {
             get;
